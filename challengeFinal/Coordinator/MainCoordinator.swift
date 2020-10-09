f0ade76ce953b1b6f4bc3b4c2faca34df09bf25a
Pietro <pietropugliesi@icloud.com>
2020-10-09 11:13:06 -0300

    organizacao em pastas e buildando

---
+++ challengeFinal/Coordinator/MainCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  MainCoordinator.swift
+//  challengeFinal
+//
+//  Created by Pietro Pugliesi on 09/10/20.
+//
+
+import Foundation
+import UIKit
+
+class MainCoordinator:Coordinator{
+    var childCoordinators = [Coordinator]()
+    
+    var  navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = ViewController()
+        navigationController.pushViewController(vc, animated: false)
+    }
+    
+    
+}
