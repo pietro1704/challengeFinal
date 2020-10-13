ce26bf158576c445b664691f297f8520ba3b0cca
Pietro <pietropugliesi@icloud.com>
2020-10-13 10:57:01 -0300

    gitignore pro mac e funcionando teste coordinator com storyboard

--- challengeFinal/Coordinator/Storyboarded.swift
+++ challengeFinal/Coordinator/Storyboarded.swift
@@ -12,11 +12,10 @@ protocol Storyboarded {
     static func instantiate()->Self
 }
 
-extension Storyboarded where Self:UIViewController{
+extension Storyboarded where Self: UIViewController{
     static func instantiate()->Self{
         let id = String(describing: self)
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         return storyboard.instantiateViewController(withIdentifier: id) as! Self
     }
-
 }
