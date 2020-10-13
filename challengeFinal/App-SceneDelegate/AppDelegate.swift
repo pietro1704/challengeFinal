a41f951eda0d8d50e7b4ec5ce2955a9d350df39b
Pietro <pietropugliesi@icloud.com>
2020-10-13 11:04:14 -0300

    arrumados espaços

--- challengeFinal/App-SceneDelegate/AppDelegate.swift
+++ challengeFinal/App-SceneDelegate/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var coordinator:MainCoordinator?
+    var coordinator: MainCoordinator?
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
