120b84db8827fac26f8c66488a95d10f534d1e15
Pietro <pietropugliesi@icloud.com>
2020-10-16 08:07:00 -0300

    test storyNode creation

--- challengeFinalTests/StoryNodesTest.swift
+++ challengeFinalTests/StoryNodesTest.swift
@@ -11,6 +11,28 @@ import Foundation
 
 class StoryNodesTest: XCTestCase {
     
+    
+    func testStoryNodesCreation() throws{
+        var node1 = StoryNode(id: 0, text: "decisão1")
+        var node2 = StoryNode(id: 1, text: "decisão2")
+        var node3 = StoryNode(id: 2, text: "decisão3")
+        var node4 = StoryNode(id: 3, text: "decisão4")
+        var node5 = StoryNode(id: 4, text: "decisão5")
+        
+        
+        var storyNodes = [node1, node2, node3, node4, node5]
+        
+        node1.childNodeIDs = [2, 3]
+        node3.childNodeIDs = [4]
+        node4.childNodeIDs = [5]
+        
+        for node in storyNodes{
+            var i = 1
+            print("node\(i): \(node)")
+            i += 1
+        }
+    }
+    
     func testParse() throws {
         let path = Bundle.main.path(forResource: "StoryNodes", ofType: "json")!
         
