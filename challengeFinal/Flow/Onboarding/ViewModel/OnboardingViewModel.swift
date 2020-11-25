d89683395d875b2934832971ff30596dc1d906fd
Guilherme Tavares Shimamoto <guilherme_shima@hotmail.com>
2020-11-25 09:29:42 -0300

    Fix in onboarding text and in story node 1

--- challengeFinal/Flow/Onboarding/ViewModel/OnboardingViewModel.swift
+++ challengeFinal/Flow/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -12,18 +12,19 @@ public class OnboardingViewModel {
     var playerService: PlayerServiceProtocol = PlayerService()
     var imagePath: String = "00_menu"
     var text = """
-Bem-vindo a Arajá!
-Sou guia de nossa humilde vila, ou era... Já nem sei mais. Bem, vou te passar algumas dicas importantes para visitar nosso território pela primeira vez.
+Bem-vindo(a) a Arajá!
 
-Aqui seus caminhos são limitados por alguns... Pontos de decisão. Aparentemente você tem 11? INCRÍVEL, você é um homem de sorte sabia?
+Sou guia de nossa humilde vila, ou era... já nem sei mais. Bem, vou te passar algumas dicas importantes para visitar nosso território pela primeira vez.
 
-Vou te contar meus segredos para utilizar esses pontos, do jeito que minha vózinha me ensinou:
+Aqui, seus caminhos são limitados por alguns... pontos de decisão. Aparentemente você tem 11. INCRÍVEL, você é uma pessoa de sorte, sabia?
 
-- Escolhas são caras, rapaz e como são caras! Cada escolha um pontinho se vai.
-- Apostar é divertido, você deveria tentar... Nada melhor do que o receber 2 pontos de decisão pelo preço de 1.
-- O último truque é meu favorito, o acaso. Não gastar nada, para viver sem amarras.
+Vou te contar meus segredos para utilizar esses pontos, do jeito que minha avozinha me ensinou:
 
-Mais uma coisa, tome muito cuidado meu rapaz, caso seus pontos acabem, pode terminar como eu... Aproveitando a vida ao máximo e em sua maior plenitude. Abençoado para não ter limites. Aliás, vou ali fazer umas flexões. Até mais. Ou não.
+• Escolhas são caras, viu? E como são caras! Cada escolha, um pontinho se vai.
+• Apostar é divertido, você deveria tentar. Escolha o caminho e faça sua aposta. Se acertar, ganha 2 pontos, se errar, perde 1. Afinal, a vida não passa de um jogo, não é mesmo?
+• O último truque é meu favorito: a aleatoriedade. Você não gasta nenhum ponto, e deixa o acaso decidir seu destino. É o segredo para viver sem amarras!
+
+Mais uma coisa: tome muito cuidado. Caso seus pontos acabem, pode terminar como eu... aproveitando a vida ao máximo e em toda sua plenitude. Abençoado por não ter limites. Aliás, vou ali fazer umas flexões. Até mais. Ou não.
 """
 
     public init(coordinatorDelegate: OnboardingViewModelCoordinatorDelegate? = nil) {
